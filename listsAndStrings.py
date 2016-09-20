fbb6f224018c5af221f09856a87b2811614d58b1
ColinWoood <colin.wood@bush.edu>
2016-09-20 14:21:25 -0700

    lists and strings

---
+++ listsAndStrings.py
@@ -0,0 +1,51 @@
+# python 2
+#
+# Name:
+#
+# Homework 2, Problem 1a and 1b
+# slicing and indexing challenges: Lists and Strings
+#
+
+pi = [3,1,4,1,5,9]
+e = [2,7,1]
+
+# Example problem (problem 0):
+# Creating the list [2,5,9] from pi and e
+answer0 = [ e[0] ] + pi[-2:]
+print answer0
+
+answer1 = e[1:]
+print answer1
+
+answer2 = pi[5::-2]
+print answer2
+
+answer3 = pi[1:]
+print answer3
+
+answer4 = e[2::-2]+pi[0::2]
+print answer4
+
+# starting strings for Homework 1
+
+h = 'harvey'
+m = 'mudd'
+c = 'college'
+
+answer5 = h[0]+h[4:]+h[5]+c[1]+m[1]
+print answer5
+
+answer6 = c[0:4]+m[1:3]+h[4]
+print answer6
+
+answer7 = h[1:]+m[1:]
+print answer7
+
+answer8 = h[0:3]+m[-1]+h[4]+h[0:3]+h[0:3]+h[0:3]
+print answer8
+
+answer9 = c[3:6]+c[1]+m[0]+h[5]+c[4:6]+c[1]
+print answer9
+
+answer10= c[0:6:2]+h[1:3]+c[0]+h[1]+c[2:4]
+print answer10
