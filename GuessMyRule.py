9c70f7dd16df4d3b7c7026bcbc1989cfbeabe3f0
ColinWoood <colin.wood@bush.edu>
2016-10-12 08:56:23 -0700

    Brush ups

--- GuessMyRule.py
+++ GuessMyRule.py
@@ -1,22 +1,26 @@
 import random
 
-
+#Sets two variables, Slope and yIntercept, to be random numbers between 1 and 10
 Slope = random.randint(1,10)
 yIntercept = random.randint(1,10)
 
+#Requests user input. Multiplies the input (an integer) by Slope and adds yIntercept to the product.
+#Asks if the user needs to try more inputs in order to guess the Slope and yIntercept
+#If the user says yes, the function will start over
+#If the user says no, it will send the user to GuessRule()
 def inputOutput():
     x = raw_input("please select a number: ")
     while x.isdigit() == False:
-        x = raw_input("Only numbers please. Please select a number: ")
+        x = raw_input("Only numbers. Please try again: ")
 
     compRule = Slope*int(x)+yIntercept
     print 'm('+str(int(x))+')+'+'b= '+str(compRule)
+    print " "
 
     y = raw_input('Do you need another guess? Enter yes or no: ')
     y = y.lower()
     while y != 'yes' and y!= 'no':
-        y = raw_input('Do you need another guess? Enter yes or no: ')
-
+        y = raw_input('Do you need to select another input? Enter yes or no: ')
 
     if y == 'yes':
         inputOutput()
@@ -24,7 +28,11 @@ def inputOutput():
         GuessRule()
 
 
-
+#Asks the user to guess what Slope is.
+#If the user guesses incorrectly, he told to try more numbers and sent back to inputOutput()
+#If the user guesses correctly, the user is asked what yIntercept is.
+#If the user guesses yIntercept incorrectly, he is told to try more numbers and sent back to inputOutput()
+#If the user is correct, the game is over, and the user is sent to playAgain()
 def GuessRule():
     z = raw_input('What do you think m is? ')
     while z.isdigit() == False:
@@ -40,6 +48,7 @@ def GuessRule():
         else:
             print 'Nope'
             print 'Try more numbers'
+            print ' '
             inputOutput()
 
     else:
@@ -47,6 +56,9 @@ def GuessRule():
         print 'Try more numbers'
         inputOutput()
 
+#Asks the user if he wants to play again
+#If yes, user is sent back to inputOutput()
+#If no, the game ends
 def  playAgain():
     m = raw_input('Would you like to play again? Enter yes or no: ')
     m = m.lower()
@@ -59,7 +71,7 @@ def  playAgain():
 
 
 
-
+#Gives user the instructions for the game and launches inputOutput()
 def instructions():
     print "hi! Let's play a game."
     print "I'm thinking of two numbers, each between 1 and 10"
@@ -67,6 +79,8 @@ def instructions():
     print "You try to guess my numbers after I put your number into the form mx+b."
     print "m and b are my numbers, while x is yours."
     print "When you think you know my numbers, tell me you don't need more guesses by typing 'no' and you can try to guess them."
+    print "You should be able to find the answer after one input for x."
+    print " "
     inputOutput()
 
 
