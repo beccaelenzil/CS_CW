58bf7ffc370f33d34b630f21dba063aa413a1f22
ColinWoood <colin.wood@bush.edu>
2016-10-24 12:39:44 -0700

    Game of Life - Print Board

---
+++ GameOfLife.py
@@ -0,0 +1,34 @@
+import random
+
+def createOneRow(width):
+    """ returns one row of zeros of width "width"...
+         You should use this in your
+         createBoard(width, height) function """
+    row = []
+    for col in range(width):
+        row += [0]
+    return row
+
+#print createOneRow(10)
+
+
+def createBoard(width, height):
+    """
+    returns "height" rows of zeros of width "width"...
+    """
+    I = []
+    for i in range(height):
+        I += [createOneRow(width)]
+    return I
+
+
+def printBoard(I):
+    for row in I:
+        line = ''
+        for col in row:
+            line += str(col)
+        print line
+
+I = createBoard(5,5)
+printBoard(I)
+
