3a571fce7b83e89663607001173d71112c91d074
ColinWoood <colin.wood@bush.edu>
2016-12-12 20:29:09 -0800

    Snow

--- ScoringTheGame/Animation.py
+++ ScoringTheGame/Animation.py
@@ -1,7 +1,9 @@
 import math
 import pygame
+import random
 pygame.init()
 
+"""
 BLACK=(0,0,0)
 WHITE=(255,255,255)
 GREEN=(0,255,0)
@@ -57,9 +59,9 @@ while not done:
         rect_y_change = rect_y_change * -1
     elif rect_x < 0:
         rect_x_change = rect_x_change * -1
-    """
 
-    """
+    screen.fill(BLACK)
+
     pygame.display.flip()
 
     # --- Limit to 60 frames per second
@@ -67,4 +69,72 @@ while not done:
 
 pygame.quit()
 
+    """
 
+import pygame
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+pygame.init()
+
+# Set the width and height of the screen [width, height]
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("My Game")
+
+# Loop until the user clicks the close button.
+done = False
+
+snow_list = []
+for i in range(50):
+    x = random.randrange(0, 700)
+    y = random.randrange(0, 500)
+    snow_list.append([x, y])
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    # --- Game logic should go here
+
+    # --- Screen-clearing code goes here
+
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+
+    # If you want a background image, replace this clear with bliting the
+    # background image.
+    screen.fill(BLACK)
+
+    # --- Drawing code should go here
+    for item in range(len(snow_list)):
+        pygame.draw.circle(screen, WHITE, snow_list[item], 2)
+        snow_list[item][1] += 1
+
+        if snow_list[item][1] > 500:
+            y = random.randrange(-50, -10)
+            snow_list[item][1] = y
+            x = random.randrange(0,400)
+            snow_list[item][0] = x
+
+
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    clock.tick(20)
+
+# Close the window and quit.
+pygame.quit()
