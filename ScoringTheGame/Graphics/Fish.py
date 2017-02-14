1671c764bd4c6fac1fffab9c0aeec57a13451be6
ColinWoood <colin.wood@bush.edu>
2017-02-14 09:41:00 -0800

    Lab Progress
    Fish Move different directions. Collisions work. scoreboard works.

--- ScoringTheGame/Graphics/Fish.py
+++ ScoringTheGame/Graphics/Fish.py
@@ -1,5 +1,5 @@
 
-
+#make it so the mouse moves slower and the player can't leave the screen
 import random
 import pygame
 
@@ -19,7 +19,7 @@ screen_height = 717
 screen = pygame.display.set_mode([screen_width, screen_height])
 pygame.display.set_caption("Fish Game")
 background = pygame.image.load("SeaBackground.png")
-
+scorecount = 0
 #plankton image
 plankton_image = pygame.image.load("Plankton.png")
 plankton_image.set_colorkey(WHITE)
@@ -36,6 +36,8 @@ leftPiranha_image = pygame.image.load("leftPiranha.png")
 
 #eaten sound
 eaten_sound = pygame.mixer.Sound('laser5.ogg')
+
+
 
 class Fish(pygame.sprite.Sprite):
     def __init__(self,aFishImage):
@@ -66,43 +68,29 @@ class Fish(pygame.sprite.Sprite):
 
 
     def reset_posXY(self,x,y):
-        """ Reset position to the top of the screen, at a random x location.
-        Called by update() or the main program loop if there is a collision.
-        """
-        self.rect.x = random.randrange(0,x)
-        self.rect.y = random.randrange(0,y)
+
+    #Reset position to the top of the screen, at a random x location.
+     #   Called by update() or the main program loop if there is a collision.
+
+        self.rect.x = x
+        self.rect.y = y
+
 
     def update(self):
         """ Called each frame. """
-
+        if self.fishType == 'plankton':
         # Move block down one pixel
-        self.rect.y += 20
+            self.rect.y += 20
 
-        # If block is too far down, reset to top of screen.
-        if self.rect.y > 750:
-            self.reset_posXY(1300,1)
+            # If block is too far down, reset to top of screen.
+            if self.rect.y > 750:
+                self.reset_posXY(random.randrange(0,screen_width),0)
 
-     #TODO option to create an updateXY(self,x,y): update top to bottom or left to right
-
-
-    # TODO def fishMove(Fish,params): that can be used by any instance(type) of FISH, player type
-
-class leftPiranha(Fish):
-
-    def reset_pos(self):
-        """ Reset position to the right of the screen, at a random y location.
-        Called by update() or the main program loop if there is a collision.
-        """
-        self.rect.y = random.randrange(0, 700)
-        self.rect.x = 1300
-
-    def update(self):
-        self.fishType = "leftPiranha"
-        self.image = leftPiranha_image
-        self.rect.x -= 20
-        self.rect.y += 1
-        if self.rect.y < 0:
-            self.reset_pos()
+        else:
+            self.rect.x -= random.randint(10,20)
+            self.rect.y += random.randint(-3,3)
+            if self.rect.x < 0:
+                self.reset_posXY(screen_width,random.randrange(0,screen_height))
 
 
 
@@ -117,12 +105,19 @@ class Player(Fish):
         # Get the current mouse position. This returns the position
         # as a list of two numbers.
         pos = pygame.mouse.get_pos()
+        pygame.mouse.set_visible(False)
 
         # Fetch the x and y out of the list,
         # just like we'd fetch letters out of a string.
         # Set the player object to the mouse location
         self.rect.x = pos[0]
         self.rect.y = pos[1]
+
+    def reset_pos(self):
+        self.rect.x = 0
+        self.rect.y = 0
+
+
 
 
 # This is a list of fish 'sprites.' Each fish in the program is
@@ -146,16 +141,14 @@ def fishList(aFishType,fishImage,aRange):
     # Add the fishType to the list of objects
     fish_list.add(aFish)
 
-
-
     # Add to all sprites
     all_sprites_list.add(aFish)
 
     #print "loop: all_sprites_list:" + str(len(all_sprites_list.sprites()))
 
 
-#create fishTypeLists
-fishList('plankton',plankton_image,10)
+
+fishList('plankton',plankton_image,1)
 fishList('leftShark',leftShark_image,5)
 fishList('leftPirahana',leftPiranha_image,5)
 
@@ -163,8 +156,7 @@ fishList('leftPirahana',leftPiranha_image,5)
 # Create a Fish.Player
 player = Player(player_image)
 all_sprites_list.add(player)
-leftPiranha = leftPiranha(leftPiranha_image)
-all_sprites_list.add(leftPiranha)
+
 
 
 """
@@ -199,7 +191,6 @@ while not done:
 
     #Copy pixels from the source surface (background_image) onto the screen
     screen.blit(background, [0, 0])
-
     # Calls update() method on every sprite in the list
     all_sprites_list.update()
 
@@ -238,6 +229,8 @@ while not done:
     fish_collided_list = pygame.sprite.spritecollide(player, fish_list, False)
 
     # Check the list of collisions for just planktons.
+    font = pygame.font.SysFont('Calibri', 50, True, False)
+
     for fish in fish_collided_list:
         if fish.fishType == "plankton":
             eaten += 1
@@ -245,14 +238,18 @@ while not done:
             eaten_sound.play()
             # Reset fish to the top of the screen to fall again.
             fish.reset_pos()
-
+            scorecount += 1
         else:
             # TODO once player has collided with a non plankton, remove it from the collision list
             print(fish.fishType + " not eaten ")
-            fish.reset_pos()
-
+            print "you lost"
+            player.reset_pos()
             #reset position for leftShark (per example)
             #fish.reset_posXY(1277,717)
+
+    scoreboard = font.render("Score: " + str(scorecount), True, BLACK)
+    screen.blit(scoreboard,[10,10])
+
 
     # Draw all the spites
     all_sprites_list.draw(screen)
